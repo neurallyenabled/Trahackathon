707661c9d30c197ae4a6bee35a26e2c163899da9
Natheer <natheer@extbh.dev>
2023-02-20 12:09:14 +0000

    Update step2.py

--- step2.py
+++ step2.py
@@ -1,46 +1,47 @@
-# sending by the smtplib
-# does not validate the email
 import socket
 import unicodedata
 import idna
 from email_validator import validate_email, EmailNotValidError
 import smtplib
-try:
-    to = "حكم-١@هتا.البحرين"
-    sndr = 'اخضر@فريق-٧.البحرين'
-    sndr_local, sndr_domain = sndr.split("@", 1)
-    sndr_normal = unicodedata.normalize('NFC', sndr_domain)
 
-    sndr = '@'.join((sndr_local, idna.encode(sndr_normal).decode("ascii")))
+def parse_main(mail):
+  mail_local, mail_domain = mail.split('@')
+  mail_normal = unicodedata.normalize('NFC', mail_domain)
+  mail_mail = '@'.join((mail_local, idna.encode(mail_normal).decode("ascii")))
+  return mail_mail
 
-    local_part, domain = to.rsplit('@', 1)
-    domain_normalized = unicodedata.normalize("NFC", domain)
-    
-    # convert U-label to A-label
-    to = '@'.join((local_part, idna.encode(domain_normalized).decode("ascii")))
-    print(to)
-    # validate email address
-    validated = validate_email(to, check_deliverability=True)
-    if validated:
-        host = '10.83.9.133'
-        port = '578'
-        smtp = smtplib.SMTP(host, port)
-        smtp.set_debuglevel(False)
-        smtp.login(sndr, '8771643373')
-        print('Logged in')
-        sender = sndr
-        subject = 'Step-2'
-        content = 'Team7'
-        msg = EmailMessage()
-        msg.set_content(content)
-        msg['Subject'] = subject
-        msg['From'] = sender
-        msg['to'] = to
-        smtp.send_message(msg, sender, to)
-        smtp.quit()
-        logger.info("Email sent to '{to}'")
-except smtplib.SMTPNotSupportedError:
-    # the server does not support the SMTPUTF8 option, you may want to perform downgrading
-    logger.warning(
-        "the SMTP server {host}:{port} does not support the SMTPUTF8 option")
-    raise
+# creates SMTP session
+s = smtplib.SMTP('10.83.9.133', 25)
+print('Connected to SMTP')
+# start TLS for security
+s.starttls()
+
+sender_mail = 'اخضر@فريق-٧.البحرين'
+to = 'حكم-١@هتا.البحرين'
+
+to = parse_main(to)
+sender_mail = parse_main(sender_mail)
+print('parsed', sender_mail, to)
+
+# s.login(sender_mail, '8771643373')
+# print('Logged in')
+
+# # message to be sent
+# header = 'To:' + to + '\n' + 'From: ' + euser + '\n' + 'Subject:Step-1 \n'
+# print (header)
+msg = f'''\
+From: {sender_mail}
+To: {to}
+Subject: Step-2
+
+فريق٧
+'''
+
+# sending the mail
+s.sendmail(sender_mail, to, msg)
+print('sent')
+# terminating the session
+s.quit()
+
+
+
