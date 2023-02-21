b8cb17b1f96e6d8fb5abb7c92a19072e7f227115
fatimaalkooheji <101482815+fatimaalkooheji@users.noreply.github.com>
2023-02-21 11:02:33 +0300

    Update arabiconverter.py

--- arabiconverter.py
+++ arabiconverter.py
@@ -1,15 +1,21 @@
 import unicodedata
 import idna
 
+
 url = input("Enter website : ")
 
 try:
     url_normalized = unicodedata.normalize('NFC',url)
     print(url_normalized)
     url_alabel = idna.encode(url_normalized).decode("ascii")
-    print(url_alabel)
+    print("The ASCII translation of it: " + url_alabel)
+
+    # Here we might can use the url_alabel in a shortening the link.  And then we assign the shortened into a variable ,
+    # Then we decode the shortened variable.
+
+
     url_ulabel = idna.decode(url_alabel)
-    print(url_ulabel)
+    print("ُThe URL is : " + url_ulabel)
 except idna.IDNAError as e:
     print(f"URL '{url} is invalid: {e}")
 except Exception as e:
